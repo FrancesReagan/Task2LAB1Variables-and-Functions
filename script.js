845e1ec05867c510643d8ea26e28535c21d1cf30
Frances Reagan James <adtgwen@gmail.com>
2025-05-13 14:12:37 -0400

    corrected javascript

---
+++ script.js
@@ -0,0 +1,38 @@
+function calculateTotalCost(price, quantity, taxRate) {
+  //check if all are valide numbers//
+  if (
+    typeof price !== 'number' || isNaN(price) ||
+    typeof quantity !== 'number' || isNaN(quantity) ||
+    typeof taxRate !== 'number' || isNaN(taxRate)
+  ){
+    return "Invalid input.";
+  }
+
+  //Total cost calculate//
+  const totalCost = (price * quantity) * (1 + taxRate);
+
+  return totalCost;
+}
+
+//Function to handle the button click//
+function handleCalculation() 
+//Get Values from input fields//
+const price = parseFloat(document.getElementById('price').value);
+const quantity = parseInt(document.getElementById('quantity').value);
+const taxRate = parseFloat(document.getElementById('taxRate').value);
+
+//Total Cost Calculate//
+const result = calculateTotalCost(price, quantity,taxRate);
+
+//Display result//
+const resultElement = document.getElementById('result');
+
+if (result === "Invalid input.") {
+  resultElement.textContent = result;
+  resultElement.style.color = '#e53e3e'; //error color Red//
+} else {
+  //result format will be currency//
+  const formattedResult = '$' + result.toFixed(2);
+  resultElement.textContent = formattedResult;
+  resultElement.style.color = '#2b6cb0'; //success blue color//
+}
