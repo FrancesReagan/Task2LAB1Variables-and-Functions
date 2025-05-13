b8ff2275a688657d7a1c37dda6e0dc9c7b05c50b
Frances Reagan James <adtgwen@gmail.com>
2025-05-13 14:31:55 -0400

    fixed missing form-group quantity in html and fixed misspelled word in CSS

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function calculateTotalCost(price, quantity, taxRate) {
   //check if all are valide numbers//
   if (
-    typeof price !== 'number' || isNaN(price) ||
-    typeof quantity !== 'number' || isNaN(quantity) ||
-    typeof taxRate !== 'number' || isNaN(taxRate)
+    typeof price !== "number" || isNaN(price) ||
+    typeof quantity !== "number" || isNaN(quantity) ||
+    typeof taxRate !== "number" || isNaN(taxRate)
   ){
     return "Invalid input.";
   }
@@ -15,24 +15,31 @@ function calculateTotalCost(price, quantity, taxRate) {
 }
 
 //Function to handle the button click//
-function handleCalculation() 
-//Get Values from input fields//
-const price = parseFloat(document.getElementById('price').value);
-const quantity = parseInt(document.getElementById('quantity').value);
-const taxRate = parseFloat(document.getElementById('taxRate').value);
+function handleCalculation() {
+  //Get Values from input fields//
+  const price = parseFloat(document.getElementById("price").value);
+  const quantity = parseInt(document.getElementById("quantity").value);
+  const taxRate = parseFloat(document.getElementById("taxRate").value);
 
-//Total Cost Calculate//
-const result = calculateTotalCost(price, quantity,taxRate);
+  //Total Cost Calculate//
+  const result = calculateTotalCost(price, quantity,taxRate);
 
-//Display result//
-const resultElement = document.getElementById('result');
+  //Display result//
+  const resultElement = document.getElementById("result");
 
-if (result === "Invalid input.") {
-  resultElement.textContent = result;
-  resultElement.style.color = '#e53e3e'; //error color Red//
-} else {
-  //result format will be currency//
-  const formattedResult = '$' + result.toFixed(2);
-  resultElement.textContent = formattedResult;
-  resultElement.style.color = '#2b6cb0'; //success blue color//
+  if (result === "Invalid input.") {
+    resultElement.textContent = result;
+    resultElement.style.color = "#e53e3e"; //error color Red//
+  } else {
+    //result format will be currency//
+    const formattedResult = "$" + result.toFixed(2);
+    resultElement.textContent = formattedResult;
+    resultElement.style.color = "#2b6cb0"; //success blue color//
+  }
 }
+
+// Event listern for when DOM is loaded to handle handleCalculation function//
+document.addEventListener("DOMContentLoaded", function() {
+  const calculateButton = document.getElementById("calculateButton");
+  calculateButton.addEventListener("click", handleCalculation);
+});
